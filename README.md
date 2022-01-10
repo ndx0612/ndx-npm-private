1ea06ae656093d40572443c14fa9e8e7232ff51b
ndx2527 <1378362527@qq.com>
2022-01-10 17:28:28 +0800

    01.10时差计算、排序

--- README.md
+++ README.md
@@ -36,3 +36,89 @@ console.log(obj2);
 
 
 ## 二、查询
+
+1、deepClone
+
+说明：深度克隆一个对象或者数组。
+
+参数：传入参数为一个对象/数组，返回一个深度克隆后的对象。
+
+示例：
+
+```javascript
+const obj1 = {
+  name: 'tom',
+  age: 20,
+  arr: [1, 2, 3, 4]
+}
+const obj2 = formatDate.deepClone(obj); // 返回深度克隆后的对象
+```
+
+
+
+2、fastSort
+
+说明：将数组从小到大排序（默认）
+
+参数：传入参数为一个数组，返回排序后的数组。
+
+​		第二个参数为bool值，默认为flase（升序）。true为降序
+
+示例：
+
+```javascript
+nums = [1, 31, 5, 20, 8, 16, 51, 64]
+fastSort(nums) // 升序
+fastSort(nums,true) // 降序
+```
+
+
+
+3、sortObj
+
+说明：对数组（里面包含多个对象）进行排序。
+
+参数：第一个为需要排序的数组对象
+
+​		第二个参数为字符串，选择需要排序的字段
+
+示例：
+
+```javascript
+list = [
+  { name: 'lwx', sex: 'bbbb', age: 22 },
+  { name: 'xiaohu', sex: 'fd', age: 30 },
+  { name: 'lwx', sex: 'ghf', age: 5 },
+  { name: 'tian', sex: 'ghf', age: 10 },
+];
+sortObj(list, 'age'); // 通过age进行排序(升序)
+```
+
+
+
+4、timediffer
+
+说明：时间差值显为易读形式。
+
+​	1.小于60s，显示刚刚
+
+​	2.大于60s，小于1个小时，？分钟前
+
+​	3.大于1小时，小于24小时，？小时前
+
+​	4.大于24小时，小于一个月，？天前
+
+​	5.大于一个月，小于12个月，？个月前
+
+​	6.大于12个月，？年前
+
+参数：第一个参数为过去事件，第二个参数为之后时间（不传默认为当前时间）
+
+示例：
+
+```javascript
+var t1 = new Date("2020-10-11 15:21:57");
+var t2 = new Date(); // 获取当前时间
+console.log(timediffer(t1, t2));
+```
+
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 深度克隆
 function deepClone(source) {
   // 如果类型不是对象
   if (typeof source != "object") {
@@ -13,7 +14,80 @@ function deepClone(source) {
   }
   return newObj;
 }
+
+// 数组排序,true为降序,不传为升序
+function fastSort(array, bool) {
+  let nums = array;
+  if (!bool) {
+    function sortNumber(a, b) {
+      return a - b;
+    }
+  } else {
+    function sortNumber(a, b) {
+      return b - a;
+    }
+  }
+  nums = nums.sort(sortNumber);
+  return nums;
+}
+
+// 数组对象排序
+function sortObj(object, option) {
+  function compare(option) {
+    return function (obj1, obj2) {
+      var val1 = obj1[option];
+      var val2 = obj2[option];
+      if (!isNaN(Number(val1)) && !isNaN(Number(val2))) {
+        val1 = Number(val1);
+        val2 = Number(val2);
+      }
+      if (val1 < val2) {
+        return -1;
+      } else if (val1 > val2) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+  return object.sort(compare(option));
+}
+
+// 时差计算
+var timediffer = function (last, now) {
+  const options = {
+    year: '年前',
+    month: '个月前',
+    day: '天前',
+    hour: '小时前',
+    minute: '分钟前',
+    second: '秒前',
+    just: '刚刚',
+  }
+  if (!now) {
+    var now = new Date();
+  }
+  const timer = (now - last) / 1000; // 获取秒数
+  let tip = '';
+  if (timer <= 0 || Math.floor(timer / 60) <= 0) { // 小于60s,刚刚
+    tip = options.just;
+  } else if (timer < 60 * 60) {
+    tip = Math.floor(timer / 60) + `${options.minute}`;
+  } else if (timer < 60 * 60 * 24) {
+    tip = Math.floor(timer / 3600) + `${options.hour}`;
+  } else if (timer < 60 * 60 * 24 * 30) {
+    tip = Math.ceil(timer / 86400) + `${options.day}`;
+  } else {
+    tip = Math.floor(timer / (86400 * 24)) + `${options.month}`;
+  }
+  return tip;
+}
+
+
 // 导出模块
 module.exports = {
   deepClone,
+  fastSort,
+  sortObj,
+  timediffer
 }
