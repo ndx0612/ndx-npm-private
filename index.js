604cf9db326094a030a5d5c9d02521386e6f7678
ndx2527 <1378362527@qq.com>
2022-04-22 11:50:03 +0800

    feat：时间格式转换、获取设备类型

--- index.js
+++ index.js
@@ -129,6 +129,60 @@ let unique = function (arr) {
   return data
 }
 
+/**
+ * @description: 时间戳转日期对象 默认当前日期
+ * @param {number} date 时间戳
+ * @param {string} pattern 时间格式
+ * @return {*}
+ */
+// formatDate(baseData.updateTime , 'yyyy-MM-dd hh:mm:ss')
+const formatDate = function (date = new Date(), pattern) {
+  if (typeof date == "string") date = Number(date)
+  if (typeof date == "string" || typeof date == "number") {
+    date = new Date((date + "").length == 10 ? date * 1000 : date)
+  }
+  pattern = pattern || DEFAULT_PATTERN
+  return pattern.replace(SIGN_REGEXP, function ($0) {
+    switch ($0.charAt(0)) {
+      case "y":
+        return padding(date.getFullYear(), $0.length)
+      case "M":
+        return padding(date.getMonth() + 1, $0.length)
+      case "d":
+        return padding(date.getDate(), $0.length)
+      case "w":
+        return date.getDay() + 1
+      case "h":
+        return padding(date.getHours(), $0.length)
+      case "m":
+        return padding(date.getMinutes(), $0.length)
+      case "s":
+        return padding(date.getSeconds(), $0.length)
+    }
+  })
+}
+
+/**
+ * @description: 获取设备类型
+ * @return { boolean }  facility:true移动端；反之PC端
+ * @return {*}
+ */
+const getDeviceType = function () {
+  const userAgent = navigator.userAgent
+  const Agents = [
+    "Android",
+    "iPhone",
+    "NokiaN9",
+    "SymbianOS",
+    "Windows Phone",
+    "iPad",
+  ]
+  const facility = Agents.some((i) => {
+    return userAgent.includes(i)
+  })
+  return facility
+}
+
 
 // 导出模块
 module.exports = {
@@ -140,4 +194,6 @@ module.exports = {
   arrDisorder,
   getRandom,
   unique,
+  formatDate,
+  getDeviceType,
 }
