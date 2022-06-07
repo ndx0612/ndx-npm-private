be0fdcba4069bee6e85f1cad636b73207745970e
ndx2527 <1378362527@qq.com>
2022-06-07 13:33:41 +0800

    fix：时间戳转日期修改

--- index.js
+++ index.js
@@ -117,6 +117,17 @@ function getRandom(num) {
  * @return {*}
  */
 // formatDate(baseData.updateTime , 'yyyy-MM-dd hh:mm:ss')
+
+const DEFAULT_PATTERN = "yyyy-MM-dd"
+const SIGN_REGEXP = /([yMdhsm])(\1*)/g
+// 个位日期补0
+function padding(s, len) {
+  len = len - (s + "").length
+  for (var i = 0; i < len; i++) {
+    s = "0" + s
+  }
+  return s
+}
 const formatDate = function (date = new Date(), pattern) {
   if (typeof date == "string") date = Number(date)
   if (typeof date == "string" || typeof date == "number") {
@@ -164,6 +175,66 @@ const getDeviceType = function () {
   return facility
 }
 
+let timeout = null;
+
+/**
+ * 防抖原理：一定时间内，只有最后一次操作，再过wait毫秒后才执行函数
+ * 
+ * @param {Function} func 要执行的回调函数 
+ * @param {Number} wait 延时的时间
+ * @param {Boolean} immediate 是否立即执行 
+ * @return null
+ */
+function debounce(func, wait = 500, immediate = false) {
+  // 清除定时器
+  if (timeout !== null) clearTimeout(timeout);
+  // 立即执行，此类情况一般用不到
+  if (immediate) {
+    var callNow = !timeout;
+    timeout = setTimeout(function () {
+      timeout = null;
+    }, wait);
+    if (callNow) typeof func === 'function' && func();
+  } else {
+    // 设置定时器，当最后一次操作后，timeout不会再被清除，所以在延时wait毫秒后执行func回调方法
+    timeout = setTimeout(function () {
+      typeof func === 'function' && func();
+    }, wait);
+  }
+}
+
+let timer, flag;
+/**
+ * 节流原理：在一定时间内，只能触发一次
+ * 
+ * @param {Function} func 要执行的回调函数 
+ * @param {Number} wait 延时的时间
+ * @param {Boolean} immediate 是否立即执行
+ * @return null
+ */
+function throttle(func, wait = 500, immediate = true) {
+  if (immediate) {
+    if (!flag) {
+      flag = true;
+      // 如果是立即执行，则在wait毫秒内开始时执行
+      typeof func === 'function' && func();
+      timer = setTimeout(() => {
+        flag = false;
+      }, wait);
+    }
+  } else {
+    if (!flag) {
+      flag = true
+      // 如果是非立即执行，则在wait毫秒内的结束处执行
+      timer = setTimeout(() => {
+        flag = false
+        typeof func === 'function' && func();
+      }, wait);
+    }
+  }
+};
+
+
 
 // 导出模块
 module.exports = {
@@ -176,4 +247,6 @@ module.exports = {
   getRandom,
   formatDate,
   getDeviceType,
+  debounce,
+  throttle,
 }
